477ba46fcfc4a33205012bb4b3c42c0a82bef06f
shugri abdulahi <shakiiraabdullahi@gmail.com>
2026-09-17 12:18:54 +0300

    homeassement

---
+++ homeassement/homeassement/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace homeassement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labeloftheweak = new System.Windows.Forms.Label();
+            this.lblofthemooh = new System.Windows.Forms.Label();
+            this.lblofthemooth = new System.Windows.Forms.Label();
+            this.labeeloftheyear = new System.Windows.Forms.Label();
+            this.txtdayoftheweak = new System.Windows.Forms.TextBox();
+            this.txtyear = new System.Windows.Forms.TextBox();
+            this.txtdayofthemonth = new System.Windows.Forms.TextBox();
+            this.txtmounth = new System.Windows.Forms.TextBox();
+            this.btnshow = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btnclouse = new System.Windows.Forms.Button();
+            this.bloutput = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labeloftheweak
+            // 
+            this.labeloftheweak.AutoSize = true;
+            this.labeloftheweak.Location = new System.Drawing.Point(282, 64);
+            this.labeloftheweak.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labeloftheweak.Name = "labeloftheweak";
+            this.labeloftheweak.Size = new System.Drawing.Size(216, 29);
+            this.labeloftheweak.TabIndex = 0;
+            this.labeloftheweak.Text = "enterdayoftheweak";
+            // 
+            // lblofthemooh
+            // 
+            this.lblofthemooh.AutoSize = true;
+            this.lblofthemooh.Location = new System.Drawing.Point(256, 120);
+            this.lblofthemooh.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblofthemooh.Name = "lblofthemooh";
+            this.lblofthemooh.Size = new System.Drawing.Size(242, 29);
+            this.lblofthemooh.TabIndex = 1;
+            this.lblofthemooh.Text = "enternameofthemooh";
+            this.lblofthemooh.Click += new System.EventHandler(this.lblofthemooh_Click);
+            // 
+            // lblofthemooth
+            // 
+            this.lblofthemooth.AutoSize = true;
+            this.lblofthemooth.Location = new System.Drawing.Point(191, 167);
+            this.lblofthemooth.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblofthemooth.Name = "lblofthemooth";
+            this.lblofthemooth.Size = new System.Drawing.Size(307, 29);
+            this.lblofthemooth.TabIndex = 2;
+            this.lblofthemooth.Text = "enterthenumericofthemooth";
+            // 
+            // labeeloftheyear
+            // 
+            this.labeeloftheyear.AutoSize = true;
+            this.labeeloftheyear.Location = new System.Drawing.Point(331, 228);
+            this.labeeloftheyear.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labeeloftheyear.Name = "labeeloftheyear";
+            this.labeeloftheyear.Size = new System.Drawing.Size(167, 29);
+            this.labeeloftheyear.TabIndex = 3;
+            this.labeeloftheyear.Text = "enteroftheyear";
+            // 
+            // txtdayoftheweak
+            // 
+            this.txtdayoftheweak.Location = new System.Drawing.Point(631, 64);
+            this.txtdayoftheweak.Name = "txtdayoftheweak";
+            this.txtdayoftheweak.Size = new System.Drawing.Size(223, 35);
+            this.txtdayoftheweak.TabIndex = 5;
+            // 
+            // txtyear
+            // 
+            this.txtyear.Location = new System.Drawing.Point(631, 222);
+            this.txtyear.Name = "txtyear";
+            this.txtyear.Size = new System.Drawing.Size(223, 35);
+            this.txtyear.TabIndex = 6;
+            // 
+            // txtdayofthemonth
+            // 
+            this.txtdayofthemonth.Location = new System.Drawing.Point(631, 167);
+            this.txtdayofthemonth.Name = "txtdayofthemonth";
+            this.txtdayofthemonth.Size = new System.Drawing.Size(223, 35);
+            this.txtdayofthemonth.TabIndex = 7;
+            // 
+            // txtmounth
+            // 
+            this.txtmounth.Location = new System.Drawing.Point(631, 117);
+            this.txtmounth.Name = "txtmounth";
+            this.txtmounth.Size = new System.Drawing.Size(223, 35);
+            this.txtmounth.TabIndex = 8;
+            // 
+            // btnshow
+            // 
+            this.btnshow.Location = new System.Drawing.Point(185, 413);
+            this.btnshow.Name = "btnshow";
+            this.btnshow.Size = new System.Drawing.Size(164, 66);
+            this.btnshow.TabIndex = 10;
+            this.btnshow.Text = "showdone";
+            this.btnshow.UseVisualStyleBackColor = true;
+            this.btnshow.Click += new System.EventHandler(this.btnshow_Click);
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(477, 413);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(164, 66);
+            this.btnclear.TabIndex = 11;
+            this.btnclear.Text = "clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // btnclouse
+            // 
+            this.btnclouse.Location = new System.Drawing.Point(820, 413);
+            this.btnclouse.Name = "btnclouse";
+            this.btnclouse.Size = new System.Drawing.Size(164, 66);
+            this.btnclouse.TabIndex = 12;
+            this.btnclouse.Text = "close";
+            this.btnclouse.UseVisualStyleBackColor = true;
+            this.btnclouse.Click += new System.EventHandler(this.btnclouse_Click);
+            // 
+            // bloutput
+            // 
+            this.bloutput.Location = new System.Drawing.Point(228, 296);
+            this.bloutput.Name = "bloutput";
+            this.bloutput.Size = new System.Drawing.Size(736, 54);
+            this.bloutput.TabIndex = 14;
+            this.bloutput.UseVisualStyleBackColor = true;
+            this.bloutput.Click += new System.EventHandler(this.bloutput_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 533);
+            this.Controls.Add(this.bloutput);
+            this.Controls.Add(this.btnclouse);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnshow);
+            this.Controls.Add(this.txtmounth);
+            this.Controls.Add(this.txtdayofthemonth);
+            this.Controls.Add(this.txtyear);
+            this.Controls.Add(this.txtdayoftheweak);
+            this.Controls.Add(this.labeeloftheyear);
+            this.Controls.Add(this.lblofthemooth);
+            this.Controls.Add(this.lblofthemooh);
+            this.Controls.Add(this.labeloftheweak);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labeloftheweak;
+        private System.Windows.Forms.Label lblofthemooh;
+        private System.Windows.Forms.Label lblofthemooth;
+        private System.Windows.Forms.Label labeeloftheyear;
+        private System.Windows.Forms.TextBox txtdayoftheweak;
+        private System.Windows.Forms.TextBox txtyear;
+        private System.Windows.Forms.TextBox txtdayofthemonth;
+        private System.Windows.Forms.TextBox txtmounth;
+        private System.Windows.Forms.Button btnshow;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btnclouse;
+        private System.Windows.Forms.Button bloutput;
+    }
+}
+
---
+++ homeassement/homeassement/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homeassement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnshow_Click(object sender, EventArgs e)
+        {
+            //create a variables
+            string dayofweak, nameofmooth,  fulldat , numericday, year;
+
+            //intail value of variables
+            dayofweak = txtdayoftheweak.Text;
+            nameofmooth = txtmounth.Text;
+            numericday = txtdayofthemonth.Text;
+            year=txtyear.Text;
+            int.Parse (txtdayofthemonth.Text);
+            int.Parse (txtyear.Text);
+            //strage process conction of full data
+            fulldat = dayofweak + " " + nameofmooth + " " + numericday + " " + year;
+            bloutput.Text = fulldat;
+
+        }
+
+        private void lblofthemooh_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            // clearing textbox and lebal
+            // clearng textbox
+            txtdayoftheweak.Clear();
+            txtmounth.Clear();
+            txtdayofthemonth.Clear();
+            txtyear.Clear();
+
+
+
+
+            // clearing label not used clear function
+            bloutput.Text = " ";
+        }
+
+        private void btnclouse_Click(object sender, EventArgs e)
+        {
+            //form close using this keyword and close function
+            this.Close();
+            
+        }
+
+        private void bloutput_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
